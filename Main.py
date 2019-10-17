8bc2c1a6f68a599134d2f54bdcfb9c123c23234e
Tarun Aditya Thurlapati <tarun.aditya@hotmail.com>
2019-10-17 06:05:50 +0530

    Initialized the Repo

---
+++ Main.py
@@ -0,0 +1,3 @@
+#this is the first file, which will be replaced by a structed folder system in the near future.
+#This is just a dummy for initializing the repo meaningfullly!
+print("Welcome to the APP")
